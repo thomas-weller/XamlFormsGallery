015bacbd478210a0bb7a8042f1cff09fe3363fd0
thomas-weller <info@thomas-weller.de>
2015-01-22 14:03:43 +0100

    ADD: Image

---
+++ XamlFormsGallery/XamlFormsGallery/ViewModels/ImageDemoViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using XamlFormsGallery.Mvvm;
+
+namespace XamlFormsGallery.ViewModels
+{
+    public class ImageDemoViewModel : ViewModelBase
+    {
+        private string _header;
+        private ImageSource _imageSource;
+
+        public string Header
+        {
+            get { return _header; }
+            private set { SetProperty(ref _header, value); }
+        }
+
+        public ImageSource ImageSource
+        {
+            get { return _imageSource; }
+            private set { SetProperty(ref _imageSource, value); }
+        }
+
+        public ImageDemoViewModel()
+        {
+            Header = "Image";
+            ImageSource = Device.OnPlatform(ImageSource.FromUri(new Uri("http://xamarin.com/images/index/ide-xamarin-studio.png")),
+                                            ImageSource.FromFile("ide_xamarin_studio.png"),
+                                            ImageSource.FromUri(new Uri("http://xamarin.com/images/index/ide-xamarin-studio.png")));
+
+        }
+    }
+}
---
+++ XamlFormsGallery/XamlFormsGallery/Views/ImageDemoView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamlFormsGallery.Views
+{
+    public partial class ImageDemoView : ContentPage
+    {
+        public ImageDemoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
