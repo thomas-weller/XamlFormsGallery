3aedc524e36d848207ec97dc455960a13e3481bb
thomas-weller <info@thomas-weller.de>
2015-01-22 07:00:48 +0100

    initial

---
+++ XamlFormsGallery/XamlFormsGallery/App.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+using XamlFormsGallery.Views;
+
+namespace XamlFormsGallery
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new HomeView());
+        }
+    }
+}
---
+++ XamlFormsGallery/XamlFormsGallery/ViewModels/HomeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XamlFormsGallery.Annotations;
+
+namespace XamlFormsGallery.ViewModels
+{
+    public class HomeViewModel : INotifyPropertyChanged
+    {
+        public ICommand NavigateCommand =
+            new Command<string>(async (s) =>
+            {
+                int x = 9;
+            });
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
