bd67d2d9a024e836009f3859ca59c16b87046e67
thomas-weller <info@thomas-weller.de>
2015-01-23 08:24:52 +0100

    ADD: TableView for menu

---
+++ XamlFormsGallery/XamlFormsGallery/ViewModels/TableViewMenuDemoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+using XamlFormsGallery.Mvvm;
+
+namespace XamlFormsGallery.ViewModels
+{
+    public class TableViewMenuDemoViewModel : ViewModelBase
+    {
+        public ICommand NavigateCommand
+        {
+            get { return new Command<string>(async s => await Navigator.PushAsync(s)); }
+        }
+    }
+}
