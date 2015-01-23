f73855e716df5274445e6222b47ad6d7f1f8279b
thomas-weller <info@thomas-weller.de>
2015-01-23 04:34:32 +0100

    ADD: Stepper

---
+++ XamlFormsGallery/XamlFormsGallery/ViewModels/StepperDemoViewModel.cs
@@ -0,0 +1,35 @@
+﻿using XamlFormsGallery.Mvvm;
+
+namespace XamlFormsGallery.ViewModels
+{
+    public class StepperDemoViewModel : ViewModelBase
+    {
+        private string _labelText;
+        private double _stepperValue;
+
+        public string LabelText
+        {
+            get { return _labelText; }
+            private set { SetProperty(ref _labelText, value); }
+        }
+
+        public double StepperValue
+        {
+            private get { return _stepperValue; }
+            set { SetProperty(ref _stepperValue, value); }
+        }
+
+        public StepperDemoViewModel()
+        {
+            PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "StepperValue")
+                {
+                    LabelText = string.Format("Stepper value is {0:F1}", StepperValue);
+                }
+            };
+
+            LabelText = "Stepper value is 0";
+        }
+    }
+}
---
+++ XamlFormsGallery/XamlFormsGallery/Views/StepperDemoView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamlFormsGallery.Views
+{
+    public partial class StepperDemoView : ContentPage
+    {
+        public StepperDemoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
