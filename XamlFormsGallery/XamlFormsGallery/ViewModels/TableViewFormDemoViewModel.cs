5f5fd0bc5a9c3a41f8789c2724aaf3cb02686a00
thomas-weller <info@thomas-weller.de>
2015-01-23 09:11:41 +0100

    ADD: TableViewForm

---
+++ XamlFormsGallery/XamlFormsGallery/ViewModels/TableViewFormDemoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XamlFormsGallery.Mvvm;
+
+namespace XamlFormsGallery.ViewModels
+{
+    public class TableViewFormDemoViewModel : ViewModelBase
+    {
+        private ImageSource _imageCellImageSource;
+
+        public ImageSource ImageCellImageSource
+        {
+            get { return _imageCellImageSource; }
+            private set { SetProperty(ref _imageCellImageSource, value); }
+        }
+
+        public TableViewFormDemoViewModel()
+        {
+            ImageCellImageSource = Device.OnPlatform(
+                ImageSource.FromUri(new Uri("http://xamarin.com/images/index/ide-xamarin-studio.png")),
+                ImageSource.FromFile("ide_xamarin_studio.png"),
+                ImageSource.FromFile("Images/ide-xamarin-studio.png"));
+        }
+    }
+}
