db8b9e3bbb33658ab7d109f03293416ee0377ccd
thomas-weller <info@thomas-weller.de>
2015-01-23 08:09:26 +0100

    ADD: ListView

---
+++ XamlFormsGallery/XamlFormsGallery/Views/ListViewDemoView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamlFormsGallery.Views
+{
+    public partial class ListViewDemoView : ContentPage
+    {
+        public ListViewDemoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
