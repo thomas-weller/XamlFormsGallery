ce9c8258457dbb3e976769665756a058fa80910f
thomas-weller <info@thomas-weller.de>
2015-01-24 07:47:28 +0100

    ADD: StackLayout

---
+++ XamlFormsGallery/XamlFormsGallery/Views/StackLayoutDemoView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamlFormsGallery.Views
+{
+    public partial class StackLayoutDemoView : ContentPage
+    {
+        public StackLayoutDemoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
