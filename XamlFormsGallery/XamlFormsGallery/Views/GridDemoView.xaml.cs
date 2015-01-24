ff19f88f87fca1fdcac425abd9e31068d849382c
thomas-weller <info@thomas-weller.de>
2015-01-24 09:01:34 +0100

    ADD: Grid

---
+++ XamlFormsGallery/XamlFormsGallery/Views/GridDemoView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamlFormsGallery.Views
+{
+    public partial class GridDemoView : ContentPage
+    {
+        public GridDemoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
